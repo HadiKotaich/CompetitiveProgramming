bcad36fb8893029f5c33fe2f918d1a528f84dde2
hadi kotaich <hadyko.hk@gmail.com>
2021-04-25 14:44:13 +0300

    bug fix sent by mira

--- c++/RangeQuery o(1).cpp
+++ c++/RangeQuery o(1).cpp
@@ -5,13 +5,13 @@ struct rangeQuery{
 	rangeQuery(vector<T> &a) {
 		n = a.size();
 		int LOGN = 1;
-		while ((1 << LOGN) < n) LOGN++;
+		while ((1 << LOGN) <= n) LOGN++;
 		val.resize(LOGN, vector<T>(n, 0));
 		
 		for (int i = 0; i < n; i++) val[0][i] = a[i];
 		for (int cur_pow = 1; cur_pow < LOGN; cur_pow++) {
 			int dist = 1 << (cur_pow - 1);
-			for (int i = 0; i + 2 * dist < n; i++) {
+			for (int i = 0; i + 2 * dist - 1 < n; i++) {
 				val[cur_pow][i] = combine(val[cur_pow - 1][i], val[cur_pow - 1][i + dist]);
 			}
 		}
